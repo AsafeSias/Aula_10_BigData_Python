18d0001af78ff7d419b383c583f33d85fb4b1e1e
AsafeSias <asafesias@gmail.com>
2025-12-16 21:40:37 -0300

    Atividads passada no fim da aula

--- atividade01.py
+++ atividade01.py
@@ -1,2 +1,21 @@
 # ATIVIDADE 01
-#VsCODE - GitHub
+
+def soma_pares(v1, v2):
+    soma = v1 + v2
+    return soma
+
+lst_pares = []
+
+for v in range(3):
+    try:
+        valor1 = float(input('Informe o primeiro valor: '))
+        valor2 = float (input('Informe o segundo valor: '))    
+    except (ValueError, TypeError):
+        print('Informe apenas números')
+        break
+    except KeyboardInterrupt:
+        print('Finalizado pelo usuário')
+    else:
+        soma = soma_pares(valor1, valor2)
+        lst_pares.append(soma)
+print(lst_pares)
